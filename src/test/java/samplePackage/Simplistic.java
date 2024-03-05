b9971bd0e8ece385e6042f86fb216f2a0b27c7d3
videomaker318 <autismhitman@gmail.com>
2024-03-05 12:03:21 +0530

    6thcommit

--- src/test/java/samplePackage/Simplistic.java
+++ src/test/java/samplePackage/Simplistic.java
@@ -5,7 +5,7 @@ import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class Simplistic {
 
-	public static void main(String[] args) {
+	public    main(String[] args) {
 		 
 		
 	 Logger logger = LoggerFactory.getLogger(Simplistic.class);
