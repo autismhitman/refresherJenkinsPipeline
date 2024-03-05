d4150483e6d70a3fd732b9c7feaeac346c86ca6f
videomaker318 <autismhitman@gmail.com>
2024-03-05 11:55:00 +0530

    6th commit

--- src/test/java/samplePackage/Simplistic.java
+++ src/test/java/samplePackage/Simplistic.java
@@ -11,12 +11,7 @@ public class Simplistic {
 	 Logger logger = LoggerFactory.getLogger(Simplistic.class);
 	 
 	 logger.debug("Started");
-	 System.out.println("This is first syso");
-	 System.out.println("Adding second syso");
-	 System.out.println("Adding third syso");
-	 
-	 System.out.println("Adding 4th syso");
-	 System.out.println("Adding 5th syso");
+ 
 	 
 	}
 
